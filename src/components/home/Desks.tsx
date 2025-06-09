fbda8380b1d994c6e703d755a9cc8f5f62b4280b
Bryan Flores <bryanf@marketingazul.com>
2025-06-09 14:47:12 -0600

    test

--- src/components/home/Desks.tsx
+++ src/components/home/Desks.tsx
@@ -7,7 +7,7 @@ import { useAuth } from "@context/AuthContext";
 import { TypeDesk } from "@utils/types";
 import { API_PATHS } from "@utils/apiPaths";
 import axiosInstance from "@utils/axiosInstance";
-import DeskForm from "@components/desk/Form";
+import DeskForm from "../desk/Form";
 import Modal from "@components/Modal";
 import Skeleton from "@components/Skeleton";
 
