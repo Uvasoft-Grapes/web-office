00e9e5db9b0d528295057744cb426cdf15e8abad
Grapes <bryanf@marketingazul.com>
2025-07-23 11:21:56 -0600

    Update apiPaths.ts

--- src/utils/apiPaths.ts
+++ src/utils/apiPaths.ts
@@ -1,5 +1,5 @@
-//! export const BASE_URL = "http://localhost:3000";
-export const BASE_URL = "https://check-rho-olive.vercel.app";
+! export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "https://check-rho-olive.vercel.app";
 
 export const API_PATHS = {
   AUTH:{
