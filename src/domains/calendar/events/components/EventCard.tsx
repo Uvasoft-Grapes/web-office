80dd0c1c6823806e3292e62fd740ff9a9e9666ed
Bryan Flores <bryanf@marketingazul.com>
2025-08-12 14:10:49 -0600

    eventos activos

--- src/domains/calendar/events/components/EventCard.tsx
+++ src/domains/calendar/events/components/EventCard.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { format, isAfter, isBefore, isSameMonth, isToday, startOfToday } from "date-fns";
+import { format, isAfter, isBefore, isSameMonth, isWithinInterval, startOfToday } from "date-fns";
 import { es } from "date-fns/locale";
 import { TypeEvent } from "@shared/utils/types";
 import { getAvatars } from "@shared/utils/avatars";
@@ -18,7 +18,7 @@ export default function EventCard({ date, event, refresh }:{ date:Date, event:Ty
     <>
       <li onClick={()=>setOpenModal(true)} className={`flex flex-col gap-3 h-full bg-secondary-light dark:bg-secondary-dark hover:bg-transparent dark:hover:bg-transparent rounded-xl py-4 border border-tertiary-light dark:border-tertiary-dark shadow-md shadow-quaternary dark:shadow-secondary-dark cursor-pointer duration-300 ${isSameMonth(start, date) && isAfter(end, startOfToday()) ? "opacity-100" : "opacity-25 hover:opacity-100" }`}>
         <span className="self-end w-fit mx-4 px-2 sm:px-4 py-0.5 rounded text-nowrap font-semibold text-xs text-blue-light dark:text-blue-dark bg-blue-light/20 dark:bg-blue-dark/20">{folder.title}</span>
-        <div className={`flex flex-col gap-0.5 px-4 mr-4 border-l-[3px] ${isBefore(start, startOfToday()) && isBefore(end, startOfToday()) ? "border-red-light dark:border-red-dark" : isToday(start) || isToday(end) ? "border-green-light dark:border-green-dark" : isSameMonth(start, date) ? "border-yellow-light dark:border-yellow-dark" : "border-primary-dark dark:border-primary-light"}`}>
+        <div className={`flex flex-col gap-0.5 px-4 mr-4 border-l-[3px] ${isBefore(start, startOfToday()) && isBefore(end, startOfToday()) ? "border-red-light dark:border-red-dark" : isWithinInterval(date, { start, end }) ? "border-green-light dark:border-green-dark" : isSameMonth(start, date) ? "border-yellow-light dark:border-yellow-dark" : "border-primary-dark dark:border-primary-light"}`}>
           <p className="line-clamp-1 font-semibold text-sm text-basic">{title}</p>
           <p className="line-clamp-1 leading-[18px] text-xs text-quaternary">{description}</p>
         </div>
